73bdd28115d479421e0eddef4afd9d2cb7a2a06a
Hugh Law <hugh.law@terminalfour.com>
2016-02-29 17:55:20 +0000

    update navigation oredring and remove unneccesary menu items

--- navigation.php
+++ navigation.php
@@ -1,14 +1,10 @@
 <?php
 echo "
-	<ul class='nav'>
-		<li class='nav'><a href='index.php'>Home</a></li>
-		<li class='nav'><a href='restaurant.php'>Restaurant</a></li>
-		<li class='nav'><a href='reviews.php'>Reviews</a></li>
-		<li class='nav'><a href='accommodation.php'>Accommodation</a></li>
-		<!--<li class='nav'><a href='beautyatthemill.php'>Beauty at The Mill</a></li>-->
-   		<li class='nav'><a href='localarea.php'>Local Area</a></li>
-   		<li class='nav'><a href='contactus.php'>Contact Us</a></li>
-		<li class='nav'><a href='http://www.themillrestaurant.com/blog'>Blog</a></li>
-	  </ul>
-	  ";
+<ul class='nav'>
+  <li class='nav'><a href='accommodation.php'>Accommodation</a></li>
+  <li class='nav'><a href='restaurant.php'>Restaurant</a></li>
+  <li class='nav'><a href='localarea.php'>Local Area</a></li>
+  <li class='nav'><a href='contactus.php'>Contact Us</a></li>
+  <li class='nav'><a href='reviews.php'>Reviews</a></li>
+</ul>";
 ?>
