327bd758102284e2b8ff01432d519282546fc81c
Hugh Law <hugh.law@terminalfour.com>
2018-02-26 20:37:15 +0000

    match git with live site

---
+++ images/header.php
@@ -0,0 +1,6 @@
+<?php
+echo "
+<div id='header'>
+    <img src='images/header.jpg' alt='Header Image of restaurant and surrounding area' usemap='#Map' />
+    <map name='Map' id='Map'><area shape='rect'  alt='Go back to home page' coords='354,35,595,119' href='/' /></map>
+</div>"; ?>
