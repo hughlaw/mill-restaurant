f055afe63fed4e249b2ca4c87d3ac43f2c9c0d79
Hugh Law <hugh.law@terminalfour.com>
2020-01-09 17:42:22 +0000

    css tweaks to add additional spacing, max width for text and image gallery

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,9 @@ import typography from '../css/typography.scss';
 import menu from './modules/menu';
 import buttons from './modules/buttons';
 
+import Siema from 'siema';
+
 menu();
 buttons();
+
+new Siema();
